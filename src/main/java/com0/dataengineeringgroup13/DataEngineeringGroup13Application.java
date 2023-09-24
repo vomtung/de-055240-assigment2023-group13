b017e60d19c2e9bf63103bc1f22fe4ccb2fe0a9d
TungVo <vomtung@gmail.com>
2023-09-24 08:25:10 +0700

    init project

---
+++ src/main/java/com0/dataengineeringgroup13/DataEngineeringGroup13Application.java
@@ -0,0 +1,13 @@
+package com0.dataengineeringgroup13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEngineeringGroup13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataEngineeringGroup13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com0/dataengineeringgroup13/DataEngineeringGroup13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com0.dataengineeringgroup13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataEngineeringGroup13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
