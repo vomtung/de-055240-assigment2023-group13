f6a757efc8535667b2517a4e72c1a6ab3e571008
TungVo <vomtung@gmail.com>
2023-09-24 10:24:09 +0700

    home-page

--- src/main/java/com0/dataengineeringgroup13/controller/HomeController.java
+++ src/main/java/com0/dataengineeringgroup13/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com0.dataengineeringgroup13.controller;
 
+import com0.dataengineeringgroup13.dto.ArticleDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String showForm() {
+    public String showForm(Model model) {
+
+        ArticleDto article1 = new ArticleDto(1L,"title 123","content23123");
+        ArticleDto article2 = new ArticleDto(1L,"title 222","content222222222");
+        model.addAttribute("artiles", List.of(article1, article2));
         return "home";
     }
 }
