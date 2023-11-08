928e38379ca0539c80fa06e62a8041ff1f920f8c
littleducati <lexuanphuck12@gmail.com>
2023-11-08 23:08:21 +0700

    University feature commit

---
+++ src/main/java/com0/dataengineeringgroup13/dto/UniversityDto.java
@@ -0,0 +1,28 @@
+package com0.dataengineeringgroup13.dto;
+
+public class UniversityDto {
+    private String universityname;
+
+    public String getUniversityname() {
+        return universityname;
+    }
+
+    public void setUniversityname(String universityname) {
+        this.universityname = universityname;
+    }
+
+    private String nation;
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    
+
+
+    
+}
